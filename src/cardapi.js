a70355cbfb7b78deba707981e661148d2a9e6465
artur-bertash <arturbertash88@gmail.com>
2025-09-29 21:48:57 -0600

    api key fixed

--- src/cardapi.js
+++ src/cardapi.js
@@ -2,7 +2,7 @@ const API_Giphy = import.meta.env.VITE_GIPHY_API_KEY
 
 
 export async function getImage(name) {
-    
+    console.log(API_Giphy)
     const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_Giphy}&q=${name}&limit=1&offset=0&rating=g&lang=en&bundle=low_bandwidth`)
     const responseData = await response.json()
     console.log(responseData)
